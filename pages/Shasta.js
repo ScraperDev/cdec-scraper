ef3eced6cc2b44aa7e5d7a752ecaedb121792471
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 14:07:39 -0800

    ShastaPage being created.

--- pages/Shasta.js
+++ pages/Shasta.js
@@ -1,9 +1,13 @@
 import fetch from 'isomorphic-unfetch'
 
-export default ShastaPage = () => (
-  <div>
-    
-  </div>
-);
+export default ShastaPage = ({ html }) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
 
-ShastaPage.getInitialProps()
+ShastaPage.getInitialProps = async ({ req }) => {
+  return 
+}
