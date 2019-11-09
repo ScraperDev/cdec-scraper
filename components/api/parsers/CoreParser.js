637b3bd5c30399e87fab9e5a088cffec5156a289
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 10:33:46 -0800

    getLastSunday filled out & imported to coreParser

--- components/api/parsers/CoreParser.js
+++ components/api/parsers/CoreParser.js
@@ -1,7 +1,11 @@
 export class CoreParser extends DOMParser {
+  // Base endpoint for scraping reservoir fields.
   baseRvUrl = 'http://cdec.water.ca.gov/dynamicapp/QueryDaily';
+
   createTargetUrl(rvTriplet) {
-    const lastSunday = getLastSunday();
+    const lastSunday = getLastSunday(); // Returns ISO-Formatted date for last sunday.
+    // Concatenates the query params to the baseUrl. There's probably a better way to do this, even in vanilla.
+    // However, this works & is clear enough.
     return `${this.baseRvUrl}?s=${rvTriplet}&end=${lastSunday}`;
   }
 }
---
+++ components/api/util/getLastSunday.js
@@ -0,0 +1,15 @@
+export const getLastSunday = () => {
+  const now = new Date();
+  const dayOfWeek = now.getDay();
+  if (dayOfWeek === 0) {
+    console.log(now.toISOString().substring(0, 10));
+    // It's sunday. Just returns the ISOString's date section
+    return now.toISOString().substring(0, 10);
+  } else {
+    // Not sunday. Check how many days ago sunday was
+    // & do the math to get that date
+    now.setDate(now.getDate() - dayOfWeek);
+    // Turns last sunday's date into ISOString & returns the date
+    return now.toISOString().substring(0, 10);
+  }
+};
