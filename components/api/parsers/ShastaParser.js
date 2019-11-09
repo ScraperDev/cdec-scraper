8b9ef2b79bf66997b645621cab2de591c056a5fe
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 10:47:07 -0800

    created base Scrape function

--- components/api/parsers/ShastaParser.js
+++ components/api/parsers/ShastaParser.js
@@ -2,5 +2,15 @@ import { CoreParser } from '.';
 export class ShastaParser extends CoreParser {
   // This Shasta-Specific Reservoir Page Parser requests the Shasta Reservoir page.
   // It also massages out the data we want from the returned HTML
-  async scrape() {}
+  async scrape() {
+    // Creates the url for this week in Shasta.
+    // (The method is inherited from CoreParser)
+    const shastaURL = this._createTargetUrl('SHA');
+    // Fetches the page.
+    const res = await fetch(shastaURL);
+    // Gets the text from the response body.
+    const html = await res.text();
+    // Logs the HTML
+    console.log(html);
+  }
 }
