6cf05e4fb11d7d5d93869e06a5c1d2dae397bb53
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 14:11:20 -0800

    shasta reservoir title added

--- pages/Shasta.js
+++ pages/Shasta.js
@@ -1,13 +1,20 @@
 import fetch from 'isomorphic-unfetch'
-
-export default ShastaPage = ({ html }) => {
+import Head from 'next/head';
+const ShastaPage = ({ html }) => {
   return (
+    <>
+    <Head>
+      <title>Shasta Reservoir</title>
+    </Head>
     <div>
       
     </div>
+    </>
   );
 }
 
 ShastaPage.getInitialProps = async ({ req }) => {
   return { };
 }
+
+export default ShastaPage;
