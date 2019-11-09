f254d83411fa56e35f5bc2c530704b7ac5150c49
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 01:02:02 -0800

    swapped empty object to null in reservoir context

--- src/data/ReservoirContext.js
+++ src/data/ReservoirContext.js
@@ -1,4 +1,4 @@
 import { createContext } from 'react';
 
-export const ReservoirContext = createContext({});
+export const ReservoirContext = createContext(null);
 export const ReservoirProvider = ReservoirContext.Provider;
