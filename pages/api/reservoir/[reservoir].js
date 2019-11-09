85e26540c7291fdbbe63e751379268b7c3890a85
Frederick Johnson <fritzj96@gmail.com>
2019-11-09 11:18:36 -0800

    working on the reservoir endpoint

--- pages/api/reservoir/[reservoir].js
+++ pages/api/reservoir/[reservoir].js
@@ -4,21 +4,26 @@ export default async (req, res) => {
   // Endpoint for scraping the CDEC reservoir pages & returning them as JSON.
 
   // Gets reservoir triplet
-  const { query: { reservoir } } = req;
+  const {
+    query: { reservoir }
+  } = req;
 
   // Initialize scraper outside of the switch.
   let scraper;
 
   // Switch statement that determines which scraper to use.
   // Each valid case instatiates the class & sets it equal to scraper var.
-  switch(reservoir) {
+  switch (reservoir) {
     case "SHA": {
       scraper = new ShastaParser();
       console.log(scraper);
+      break;
     }
-    case "CLE": {
-      // scraper = TrinityParser();
+    default: {
+      scraper = null;
     }
   }
-  res.end(`${reservoir} endpoint`);
-}
+  if (!scraper) {
+  } else {
+  }
+};
