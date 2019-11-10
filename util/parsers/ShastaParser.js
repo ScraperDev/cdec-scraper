372d5746d6782eea1bd21e8397567433e3f189e8
Frederick Johnson <fritzj96@gmail.com>
2019-11-10 09:41:12 -0800

    created a "createDayObject" function.

--- util/parsers/ShastaParser.js
+++ util/parsers/ShastaParser.js
@@ -16,8 +16,26 @@ export default class ShastaParser extends DOMParser {
     // Slices off the first 16 items that I don't care about.
     const dataArray = fontArray.slice(16);
 
-    // Creates a two-dimensional array, with one dimension being "data types"
-    // and the other being "date"
     console.log(dataArray);
+
+    // Takes an array sliced from dataArray & returns an object with the corresponding keys.
+    const createDayObject = (dataSlice) => {
+      return {
+        resEle: arraySlice[0],
+        storage: arraySlice[1],
+        resChg: arraySlice[2],
+        tocSto: arraySlice[3],
+        abvToc: arraySlice[4],
+        outflow: arraySlice[5],
+        inflow: arraySlice[6],
+        evap: arraySlice[7],
+        enf: arraySlice[8],
+        rivRel: arraySlice[9],
+        pptInc: arraySlice[10],
+        rain: arraySlice[11],
+        spill: arraySlice[12],
+        disPwr: arraySlice[13]
+      }
+    }
   }
 }
